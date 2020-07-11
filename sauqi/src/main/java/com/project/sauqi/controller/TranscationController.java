363165475c5875b2ea811e76d52c5523c8e857e2
Bagas Yafitra Pandji <34235801+bagasnk@users.noreply.github.com>
2020-07-11 23:34:37 +0700

    add UserHistory

--- sauqi/src/main/java/com/project/sauqi/controller/TranscationController.java
+++ sauqi/src/main/java/com/project/sauqi/controller/TranscationController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.sauqi.dao.TransactionDetailsRepo;
 import com.project.sauqi.dao.TransactionRepo;
 import com.project.sauqi.dao.UserRepo;
-import com.project.sauqi.entity.Product;
 import com.project.sauqi.entity.Transaction;
 import com.project.sauqi.entity.User;
 
@@ -36,9 +36,9 @@ public class TranscationController {
     }
     
     @GetMapping("/user/{userId}")
-    public Iterable<Transaction> getUserTransactions(@PathVariable int userId){
+    public Iterable<Transaction> getUserTransactions(@PathVariable int userId, @RequestParam String status){
         User findUser = userRepo.findById(userId).get();
-        return findUser.getTranscation();
+        return transactionRepo.findTransaksiByStatus(userId, status);
     }
     
     
--- sauqi/src/main/java/com/project/sauqi/dao/TransactionRepo.java
+++ sauqi/src/main/java/com/project/sauqi/dao/TransactionRepo.java
@@ -1,11 +1,16 @@
 package com.project.sauqi.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.project.sauqi.entity.Product;
 import com.project.sauqi.entity.Transaction;
 
 public interface TransactionRepo extends JpaRepository <Transaction, Integer>{
 	
+	@Query(value = "select * from transaction where users_id = ?1 and status = ?2",nativeQuery = true)
+	public Iterable<Transaction> findTransaksiByStatus(int id, String productName);
 
 }
