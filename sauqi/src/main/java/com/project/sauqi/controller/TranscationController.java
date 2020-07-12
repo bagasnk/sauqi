0410d55362675258bd7f4db18947d57eb6debbc0
Bagas Yafitra Pandji <34235801+bagasnk@users.noreply.github.com>
2020-07-13 03:26:45 +0700

    Add Upload Bukti Pembayaran DLL

--- sauqi/src/main/java/com/project/sauqi/controller/TranscationController.java
+++ sauqi/src/main/java/com/project/sauqi/controller/TranscationController.java
@@ -1,18 +1,41 @@
 package com.project.sauqi.controller;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.sauqi.dao.TransactionDetailsRepo;
 import com.project.sauqi.dao.TransactionRepo;
 import com.project.sauqi.dao.UserRepo;
+import com.project.sauqi.entity.Product;
 import com.project.sauqi.entity.Transaction;
 import com.project.sauqi.entity.User;
 
@@ -20,6 +43,8 @@ import com.project.sauqi.entity.User;
 @RequestMapping("/transaction")
 @CrossOrigin
 public class TranscationController {
+	
+	private String uploadPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\buktiTransfer\\";
 
 	@Autowired
 	private UserRepo userRepo;
@@ -30,24 +55,85 @@ public class TranscationController {
     @Autowired
     private TransactionRepo transactionRepo;
     
-    @GetMapping
-    public Iterable<Transaction> getAllTransaction(){
-        return transactionRepo.findAll();
-    }
+//    @GetMapping
+//    public Iterable<Transaction> getAllTransaction(){
+//        return transactionRepo.findAll();
+//    }
     
     @GetMapping("/user/{userId}")
     public Iterable<Transaction> getUserTransactions(@PathVariable int userId, @RequestParam String status){
         User findUser = userRepo.findById(userId).get();
-        return transactionRepo.findTransaksiByStatus(userId, status);
+        return transactionRepo.findTransaksiByUserIdAndStatus(userId, status);
     }
     
-    
+    @GetMapping
+    public Iterable<Transaction> getTransactions(@RequestParam String status){
+    	return transactionRepo.findTransaksiByStatus(status);
+    }
     
     @PostMapping("/addTransaction/{userId}")
 	public Transaction addTransaction(@RequestBody Transaction transaction,@PathVariable int userId) {
         User findUser = userRepo.findById(userId).get();
         transaction.setUser(findUser);
 		return transactionRepo.save(transaction);
+	}
+    
+    @PatchMapping("/editTransaction/{transactionId}")
+    public Transaction editTransaction(@RequestBody Transaction transaction,@PathVariable int transactionId) {
+    	Transaction findTransaction = transactionRepo.findById(transactionId).get();
+    	findTransaction.setStatus(transaction.getStatus());
+    	findTransaction.setTanggalAcc(transaction.getTanggalAcc());
+    	return transactionRepo.save(findTransaction);
+    }
+    
+    @PutMapping("/uploadBukti/{transactionId}")
+	public Transaction  uploadBuktiTransfer(@RequestParam ("file") Optional<MultipartFile> file, @PathVariable int transactionId ) {
+		
+		Transaction findTransaction = transactionRepo.findById(transactionId).get();
+		Date date = new Date();
+		
+		String fileDownloadUri = "no image";
+		
+		if (file.toString()!="Optional.empty") {
+			String fileExtension = file.get().getContentType().split("/")[1];
+			String newFileName = "TRF-"+ date.getTime()+ "." + fileExtension;
+			
+			String fileName = StringUtils.cleanPath(newFileName);
+			
+			Path path = Paths.get(StringUtils.cleanPath(uploadPath) + fileName);
+			
+			try {
+				Files.copy(file.get().getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+
+			fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/transaction/download/")
+					.path(fileName).toUriString();
+		}
+		findTransaction.setId(findTransaction.getId());
+		findTransaction.setStatus("pending");
+		findTransaction.setBuktiTrf(fileDownloadUri);
+		findTransaction.setTransactionDetails(findTransaction.getTransactionDetails());
+		transactionRepo.save(findTransaction);
+		return transactionRepo.save(findTransaction);
+	}
+    
+    @GetMapping("/download/{fileName:.+}")
+	public ResponseEntity<Object> downloadFile(@PathVariable String fileName){
+		Path path = Paths.get(uploadPath, fileName);
+		Resource resource = null;
+		
+		try {
+			resource = new UrlResource(path.toUri());
+		}catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+			return ResponseEntity.ok().contentType(MediaType.parseMediaType("application/octet-stream"))
+					.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+					.body(resource);
+		
 	}
     
     
--- sauqi/src/main/java/com/project/sauqi/dao/TransactionRepo.java
+++ sauqi/src/main/java/com/project/sauqi/dao/TransactionRepo.java
@@ -11,6 +11,9 @@ import com.project.sauqi.entity.Transaction;
 public interface TransactionRepo extends JpaRepository <Transaction, Integer>{
 	
 	@Query(value = "select * from transaction where users_id = ?1 and status = ?2",nativeQuery = true)
-	public Iterable<Transaction> findTransaksiByStatus(int id, String productName);
+	public Iterable<Transaction> findTransaksiByUserIdAndStatus(int id, String status);
+	
+	@Query(value = "select * from transaction where status = ?1",nativeQuery = true)
+	public Iterable<Transaction> findTransaksiByStatus(String status);
 
 }
